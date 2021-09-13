ad1c38a3bc49a992e1b4ce4712f4e5c1d13a9706
7sodikov <78784540+7sodikov@users.noreply.github.com>
2021-09-13 15:04:42 +0500

    Added new files

--- MySlidingCards/ViewController.swift
+++ MySlidingCards/ViewController.swift
@@ -6,13 +6,63 @@
 //
 
 import UIKit
+import CardSlider
 
-class ViewController: UIViewController {
+struct Item: CardSliderItem {
+    var image: UIImage
+    var rating: Int?
+    var title: String
+    var subtitle: String?
+    var description: String?
+}
+
+class ViewController: UIViewController, CardSliderDataSource {
+    
+    @IBOutlet var myButton: UIButton!
+    
+    var data = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        data.append(Item(image: UIImage(systemName: "bookmark")!,
+                         rating: nil,
+                         title: "Bookmarks",
+                         subtitle: "Get excited about bookmarks",
+                         description: "You can add bookmarks in the app now"))
+        
+        data.append(Item(image: UIImage(systemName: "bookmark")!,
+                         rating: nil,
+                         title: "Bookmarks",
+                         subtitle: "Get excited about bookmarks",
+                         description: "You can add bookmarks in the app now"))
+        
+        data.append(Item(image: UIImage(systemName: "bookmark")!,
+                         rating: nil,
+                         title: "Bookmarks",
+                         subtitle: "Get excited about bookmarks",
+                         description: "You can add bookmarks in the app now"))
+        
+        myButton.backgroundColor = .link
+        myButton.setTitleColor(.white, for: .normal)
     }
+    
+    @IBAction func didTapButton() {
+        // Present the card slider
+        let vc = CardSliderViewController.with(dataSource: self)
+        vc.title = "Welcome!"
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    
+    func item(for index: Int) -> CardSliderItem {
+        return data[index]
+    }
+    
+    func numberOfItems() -> Int {
+        return data.count
+    }
+    
 
 
 }
